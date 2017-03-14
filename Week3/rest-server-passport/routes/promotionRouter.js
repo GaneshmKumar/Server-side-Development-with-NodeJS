c88a5aa7a8936f1fb4db9c9254392cac3d2a5650
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-03-15 00:23:39 +0530

    Updated promotion router

--- Week3/rest-server-passport/routes/promotionRouter.js
+++ Week3/rest-server-passport/routes/promotionRouter.js
@@ -2,46 +2,71 @@
 * @Author: GaNeShKuMaRm
 * @Date:   2017-02-28 18:15:05
 * @Last Modified by:   GaNeShKuMaRm
-* @Last Modified time: 2017-03-15 00:11:55
+* @Last Modified time: 2017-03-15 00:23:13
 */
-
-'use strict';
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var Verify = require('./verify');
-var promotionRouter = express.Router();
+
+var Promotions = require('../models/promotions');
 
 var promotionRouter = express.Router();
+promotionRouter.use(bodyParser.json());
+
 promotionRouter.route('/')
-.all(function(req, res, next) {
-    res.writeHead(200,  {'Content-Type': 'text/plain'});
-    next();
-})
-.get(function(req, res, next) {
-    res.end('Will send all the promotions to you');
-})
-.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
-    res.end('Will add the promotion: ' + req.body.name + ' with detials: ' + req.body.description);
-})
-.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
-    res.end('Deleting promotion: ' + req.params.promotionId);
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Promotions.find({}, function (err, Promotion) {
+        if (err) throw err;
+        res.json(Promotion);
+    });
 })
 
-promotionRouter.route('/:promotionId')
-.all(function(req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    next();
+.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.create(req.body, function (err, Promotion) {
+        if (err) throw err;
+        console.log('Promotion created!');
+        var id = Promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the Promotion with id: ' + id);
+    });
 })
-.get(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
-    res.end('Will send details of the promotion: ' + req.params.promotionId + ' to you!');
-})
-.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
-    res.write('Updating the promotions ' + req.params.promotionId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-})
-.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
-    res.end('Deleting promotion: ' + req.params.promotionId);
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
 });
+
+promotionRouter.route('/:PromotionId')
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Promotions.findById(req.params.PromotionId, function (err, Promotion) {
+        if (err) throw err;
+        res.json(Promotion);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function (req, res, next) {
+    Promotions.findByIdAndUpdate(req.params.PromotionId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, Promotion) {
+        if (err) throw err;
+        res.json(Promotion);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin,function (req, res, next) {
+    Promotions.findByIdAndRemove(req.params.PromotionId, function (err, resp) {        if (err) throw err;
+        res.json(resp);
+    });
+});
+
 
 module.exports = promotionRouter;
